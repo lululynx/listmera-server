ec7c39ee3186b556c5d71342eaedaa85887d4a96
Chad Hamre <chadhamre@gmail.com>
2017-11-15 18:18:59 +0100

    heroku

--- models/playlistModels/redis.js
+++ models/playlistModels/redis.js
@@ -1,5 +1,12 @@
-const redis = require('redis');
-const client = redis.createClient();
+if (process.env.REDISTOGO_URL) {
+  const rtg = require("url").parse(process.env.REDISTOGO_URL);
+  const redis = require("redis")
+  const client = redis.createClient(rtg.port, rtg.hostname);
+  redis.auth(rtg.auth.split(":")[1]);
+} else {
+  const redis = require('redis');
+  const client = redis.createClient();
+}
 
 client.on('error', err => {
   console.error(err);
